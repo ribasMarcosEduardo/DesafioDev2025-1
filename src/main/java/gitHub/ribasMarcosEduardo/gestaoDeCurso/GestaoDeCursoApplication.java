1a8bf890a19500fa9dbfe907a4f221a15fb03798
unknown <ribas.marcoseduardo@gmail.com>
2025-01-24 19:21:51 -0300

    Base

---
+++ src/main/java/gitHub/ribasMarcosEduardo/gestaoDeCurso/GestaoDeCursoApplication.java
@@ -0,0 +1,13 @@
+package gitHub.ribasMarcosEduardo.gestaoDeCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gitHub/ribasMarcosEduardo/gestaoDeCurso/GestaoDeCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package gitHub.ribasMarcosEduardo.gestaoDeCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
