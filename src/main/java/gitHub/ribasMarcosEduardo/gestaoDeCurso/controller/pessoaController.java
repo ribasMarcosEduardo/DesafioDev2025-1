9e4b0ffba6de0aca18da70fbfd25c4a91a61a001
unknown <ribas.marcoseduardo@gmail.com>
2025-01-25 15:07:41 -0300

    frontPadao

--- src/main/java/gitHub/ribasMarcosEduardo/gestaoDeCurso/controller/pessoaController.java
+++ src/main/java/gitHub/ribasMarcosEduardo/gestaoDeCurso/controller/pessoaController.java
@@ -4,24 +4,29 @@ import gitHub.ribasMarcosEduardo.gestaoDeCurso.controller.DTO.PessoaDTO;
 import gitHub.ribasMarcosEduardo.gestaoDeCurso.entity.Pessoa;
 import gitHub.ribasMarcosEduardo.gestaoDeCurso.service.PessoaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/products")
+@Controller
+@RequestMapping("/pessoa")
 public class pessoaController {
 
     private final PessoaService pessoaService;
 
     @PostMapping("salvarPessoa")
-    public String salvarPessoa(@ModelAttribute PessoaDTO pessoaDTO) {
-        System.out.println("PessoaDTO - Usuário: " + pessoaDTO.usuario()+", Ativo: " + pessoaDTO.ativo());
+    public String salvarPessoa(@ModelAttribute PessoaDTO pessoaDTO, RedirectAttributes redirectAttributes) {
+        System.out.println("PessoaDTO - Usuário: " + pessoaDTO.usuario() + ", Ativo: " + pessoaDTO.ativo());
         Pessoa pessoa = pessoaDTO.mapearPessoa();
         pessoaService.salvarProduto(pessoa);
-        return "redirect:http://localhost:8080/menuPrincipal";
+        redirectAttributes.addFlashAttribute("mensagemSucesso", "Pessoa salva com sucesso!");
+        return "redirect:/cadastroPessoa";
     }
 
 
